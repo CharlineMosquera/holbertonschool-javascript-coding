2a554a6df16a22268f33453839d278598844f6f0
CharlineMosquera <char951144@gmail.com>
2023-09-19 18:59:11 -0500

    script that display the status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -6,6 +6,6 @@ request.get(apiUrl, (err, response) => {
   if (err) {
     console.error(err);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
